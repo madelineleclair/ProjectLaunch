85357b9e79fd9a24a53db2956f38ef03be223df9
Madeline LeClair <madelineleclair@gmail.com>
2017-04-19 23:05:31 -0400

    Make seeds for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: "Guest", email: "guest@gmail.com", password: "password")
+
+100.times do
+  User.create(name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Pokemon.name )
+end
