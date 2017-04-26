3a970a84591ca0aecbe15d23fb602f3ba814eae2
Madeline LeClair <madelineleclair@gmail.com>
2017-04-26 01:53:43 -0400

    Fix error in progress bar calculation

--- frontend/components/project/home_page/progress_bar.jsx
+++ frontend/components/project/home_page/progress_bar.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const ProgressBar = (props) => {
 
   const percentage = (props.funding / props.goal) * 100;
-  
+    debugger
   return (
     <div className="progress-bar">
-      <div className="percentage-pledged" style={{width: percentage}} />
+      <div className="percentage-pledged" style={{width: `${percentage}%`}} />
     </div>
   );
 };
