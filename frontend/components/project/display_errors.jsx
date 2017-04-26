c69da3d631051b150fa4b49c9e4c0ca4a0b3ec03
Madeline LeClair <madelineleclair@gmail.com>
2017-04-26 19:33:53 -0400

    Style errors for create projects page

--- frontend/components/project/display_errors.jsx
+++ frontend/components/project/display_errors.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const DisplayErrors = (props) => {
 
   const errors = props.errors.map((error, index) => {
-    return (<li key={index}>{error}</li>);
+    return (<li key={ index }>{ error }</li>);
   });
 
   return(
-        <ul className="errors">
-          {errors}
-        </ul>
-  )
-}
+    <ul className="errors">
+      { errors }
+    </ul>
+  );
+};
 
-export default DisplayErrors
+export default DisplayErrors;
