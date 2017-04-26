b2d81d42bd20d96b07cb24b3f46539de23c05e06
Madeline LeClair <madelineleclair@gmail.com>
2017-04-26 14:35:04 -0400

    Add css to rewards bar

--- frontend/components/project/project_show/rewards_show_list.jsx
+++ frontend/components/project/project_show/rewards_show_list.jsx
@@ -7,14 +7,15 @@ const RewardsShowList = (props) => {
   debugger
   return (
     <li key={reward.id} className="rewards-list-item">
-      <h2>Pledge ${reward.pledge_amount} or more</h2>
-      <h3>{reward.title}</h3>
-      <p>{reward.description}</p>
-      <div className="delivery-info">
-        <p>ESTIMATED DELIVERY</p>
-        <p>{deliveryDate}</p>
-      </div>
-      <p>backers</p>
+      <div>Select this reward</div>
+        <h2>Pledge ${reward.pledge_amount} or more</h2>
+        <h3>{reward.title}</h3>
+        <p>{reward.description}</p>
+        <div className="delivery-info">
+          <p>ESTIMATED DELIVERY</p>
+          <p>{deliveryDate}</p>
+          <p>backers</p>
+        </div>
     </li>
   );
 };
