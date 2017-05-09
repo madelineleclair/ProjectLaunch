fc95f17499a61a12656bca8e2f26dfcc2943aaee
Madeline LeClair <madelineleclair@gmail.com>
2017-05-08 23:05:19 -0400

    Add conditional for logger to file

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,11 +3,18 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import RootReducer from "../reducers/root_reducer";
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const createLogger = require('redux-logger');
+  middlewares.push(logger);
+}
+
 const configureStore = (preloadedState = {}) => {
   return createStore(
     RootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middlewares)
   );
 };
 
