af84b016ac44aab30578007c33e7e78bfffc12fe
Madeline LeClair <madelineleclair@gmail.com>
2017-05-11 16:52:44 -0400

    Add button to escape search bar

--- frontend/components/navigation/search_bar.jsx
+++ frontend/components/navigation/search_bar.jsx
@@ -7,6 +7,7 @@ class SearchBar extends React.Component {
     this.state = { search_term: "" };
     this.handleSearchTerm = this.handleSearchTerm.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.changeSearchStatus = this.changeSearchStatus.bind(this);
   }
 
   handleSearchTerm(e) {
@@ -14,13 +15,17 @@ class SearchBar extends React.Component {
     this.setState({ search_term });
   }
 
+  changeSearchStatus(e) {
+    this.props.changeSearchBarStatus(false);
+  }
+
   handleSubmit() {
     this.props.fetchProjects({ fetchType: "search",
     searchTerm: this.state.search_term })
     .then((() => {
       hashHistory.push('/projects/search-results');}).bind(this));
-      this.props.changeSearchBarStatus(false)
-      this.setState({ search_term: "" })
+      this.props.changeSearchBarStatus(false);
+      this.setState({ search_term: "" });
   }
 
   render() {
@@ -28,9 +33,10 @@ class SearchBar extends React.Component {
       <div className="search-bar-container nav-bar">
         <img src="https://s3.us-east-2.amazonaws.com/project-launch-dev/seeds/magnifying_glass_icon.png" />
         <form className="search-form">
-          <input onChange={ this.handleSearchTerm } type="text" placeholder="Search..." value={ this.state.search_term } />
+          <input onChange= { this.handleSearchTerm } type="text" placeholder="Search..." value={ this.state.search_term } />
           <input onClick = {this.handleSubmit} type="submit" />
         </form>
+        <button onClick= { this.changeSearchStatus }>X</button>
       </div>
     );
   }
