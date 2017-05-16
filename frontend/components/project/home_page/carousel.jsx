b55f0ccb63b1c5f516da6c949e1faf3f828c364a
Madeline LeClair <madelineleclair@gmail.com>
2017-05-15 22:09:05 -0400

    Add slider arrows

--- frontend/components/project/home_page/carousel.jsx
+++ frontend/components/project/home_page/carousel.jsx
@@ -1,58 +1,13 @@
 import React from 'react';
-// import Carousel from 'react-responsive-carousel';
-import Slider from 'react-slick'
+import Slider from 'react-slick';
 
-// class Carousel extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     const first = <img src={window.lexus_hover}/>;
-//     const second = <img src={window.alinea}/>;
-//     const third = <img src={window.code_monkey_island} />;
-//     this.state = {number: 0, images: [first, second, third]};
-//     this.nextPhoto = this.nextPhoto.bind(this);
-//     this.previousPhoto = this.previousPhoto.bind(this);
-//   }
-//
-//
-//   nextPhoto() {
-//     let number = this.state.number;
-//     if (number === this.state.images.length - 1) {
-//       this.setState({number: 0});
-//     } else {
-//       number += 1;
-//       this.setState({number});
-//     }
-//   }
-//
-//   previousPhoto() {
-//     let number = this.state.number;
-//     if (number === 0) {
-//       this.setState({number: this.state.images.length - 1});
-//     } else {
-//       number -= 1;
-//       this.setState({number});
-//     }
-//   }
-//
-//   render() {
-//     return (
-//       <div onClick={this.changePhoto} className='slider'>
-//         {this.state.images[this.state.number]}
-//         <div>
-//           <button onClick={this.previousPhoto} className="previous-button">Previuos</button>
-//           <button onClick={this.nextPhoto} className="next-button">Next</button>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-//
 const Carousel = () => {
   const settings = {
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
-    arrows: false,
+    // autoplay: true,
+    arrows: true,
+    dots: true,
     infinite: true,
   };
 
@@ -60,11 +15,13 @@ const Carousel = () => {
     <div className='outer-slider-container'>
         <Slider {...settings}>
           <img src={window.lexus_hover} />
-            <img src={window.alinea}/>
-            <img src={window.code_monkey_island} />
+          <img src={window.alinea}/>
+          <img src={window.code_monkey_island} />
         </Slider>
     </div>
   );
 };
 
 export default Carousel;
+
+// <button onClick={leftClick} className="left-button">left</button>
