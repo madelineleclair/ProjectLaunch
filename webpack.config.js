fd8af542e26e94466f22dae8b8795536578667aa
Madeline LeClair <madelineleclair@gmail.com>
2017-05-16 23:51:43 -0400

    Uncomment code to eliminate errors in live site

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
 var path = require("path");
-// var webpack = require("webpack");
+var webpack = require("webpack");
 
-// var plugins = [];
-// var devPlugins = [];
-//
-// var prodPlugins = [
-//   new webpack.DefinePlugin({
-//     'process.env': {
-//       'NODE_ENV': JSON.stringify('production')
-//     }
-//   }),
-//   new webpack.optimize.UglifyJsPlugin({
-//     compress: {
-//       warnings: true
-//     }
-//   })
-// ];
-//
-// plugins = plugins.concat(
-//   process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
-// );
+var plugins = [];
+var devPlugins = [];
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
 
 
 module.exports = {
@@ -29,7 +29,7 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js',
   },
-  // plugins: plugins,
+  plugins: plugins,
   module: {
     loaders: [
       {
