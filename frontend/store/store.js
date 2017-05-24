b50708ad915b9d39a324741264bb714cc6dda059
Madeline LeClair <madelineleclair@gmail.com>
2017-05-23 21:41:20 -0400

    Remove logger to try and eliminate push error

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,14 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import RootReducer from "../reducers/root_reducer";
 
 const middlewares = [thunk];
 
-if (process.env.NODE_ENV !== 'production') {
-  const createLogger = require('redux-logger');
-  middlewares.push(logger);
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   const createLogger = require('redux-logger');
+//   middlewares.push(logger);
+// }
 
 const configureStore = (preloadedState = {}) => {
   return createStore(
