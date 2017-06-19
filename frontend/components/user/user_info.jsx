e5f980f96f932f4a731f40b064410a3fc9b19317
Madeline LeClair <madelineleclair@gmail.com>
2017-06-19 18:16:23 -0400

    Break UserInfo to a class

--- frontend/components/user/user_info.jsx
+++ frontend/components/user/user_info.jsx
@@ -1,23 +1,49 @@
 import React from 'react';
 import { hashHistory } from 'react-router';
 
-const logOut = (props) => (e) => {
-  props.userInfo.logOut().then(() => {
-    props.setState({displayInfo: false});
-    hashHistory.push('/');
-  });
-};
 
-const UserInfo = (props) => {
+class UserInfo extends React.Component {
+  constructor(props) {
+    debugger
+    super(props);
+    this.logOut = this.logOut.bind(this);
+  }
 
-  return (
-    <div className="user-info-box">You are logged in as
-      <div>
-        {props.userInfo.currentUser.name}
+  logOut(e) {
+    this.props.userInfo.logOut().then(() => {
+      this.props.setState({displayInfo: false});
+    });
+  }
+
+  render() {
+    return (
+      <div className="user-info-box">You are logged in as
+        <div>
+          {this.props.userInfo.currentUser.name}
+        </div>
+        <button onClick={this.logOut}>Log out</button>
       </div>
-      <button onClick={logOut(props)}>Log out</button>
-    </div>
-  );
-};
+    );
+  }
+}
+
+// const logOut = (props) => (e) => {
+//   props.userInfo.logOut().then(() => {
+//     props.setState({displayInfo: false});
+//     hashHistory.push('/');
+//   });
+// };
+
+// const UserInfo = (props) => {
+//
+//   return (
+//     <div className="user-info-box">You are logged in as
+//       <div>
+//         {props.userInfo.currentUser.name}
+//       </div>
+//       <button onClick={logOut(props)}>Log out</button>
+//     </div>
+//   );
+// };
 
 export default UserInfo;
