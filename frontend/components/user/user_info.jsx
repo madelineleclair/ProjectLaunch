b64f2bf32f21d8abd85f1f17af4434e969f60466
Madeline LeClair <madelineleclair@gmail.com>
2017-07-09 14:24:57 -0400

    Delete dead code

--- frontend/components/user/user_info.jsx
+++ frontend/components/user/user_info.jsx
@@ -27,23 +27,4 @@ class UserInfo extends React.Component {
   }
 }
 
-// const logOut = (props) => (e) => {
-//   props.userInfo.logOut().then(() => {
-//     props.setState({displayInfo: false});
-//     hashHistory.push('/');
-//   });
-// };
-
-// const UserInfo = (props) => {
-//
-//   return (
-//     <div className="user-info-box">You are logged in as
-//       <div>
-//         {props.userInfo.currentUser.name}
-//       </div>
-//       <button onClick={logOut(props)}>Log out</button>
-//     </div>
-//   );
-// };
-
 export default UserInfo;
